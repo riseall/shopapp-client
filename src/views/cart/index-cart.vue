dd2415bf132406ea93c3f185463256b0feb706c1
Rizal <rizalnug2@gmail.com>
2025-01-07 18:27:19 +0700

    membuat file yang dibutuhkan

---
+++ src/views/cart/index-cart.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Shopping Cart</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
---
+++ src/views/product/detail-prod.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Product Detail</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
